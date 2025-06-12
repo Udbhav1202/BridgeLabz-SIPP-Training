fbf93a8fee66123ea6af7c498a6c02dab63930c4
Udbhav <101322451+Usri12@users.noreply.github.com>
2025-06-12 22:01:33 +0530

    Add Day1 Level1 practice problems

---
+++ src/Day1/level1/KmToMiles.java
@@ -0,0 +1,8 @@
+package Day1.level1;
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        System.out.println("The distance 10.8 Km in miles is "+(10.8*1.6));
+    }
+    
+}
---
+++ src/Day1/level1/SamAvgMarks.java
@@ -0,0 +1,8 @@
+package Day1.level1;
+
+public class SamAvgMarks {
+    public static void main(String[] args) {
+        System.out.println(" Sam’s average mark in PCM is "+((94+95+96)/3));
+    }
+}
+
