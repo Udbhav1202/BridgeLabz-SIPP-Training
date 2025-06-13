e9dfbf828f9dc4228ee36697f7ea4c25c7ee7b61
Udbhav <101322451+Usri12@users.noreply.github.com>
2025-06-13 22:55:09 +0530

    Udbhav Srivastava Update CheckNumNegOrPos and add SpringSeason solution in ControlFlows level1

--- src/ControlFlows/Leve1/CheckNumNegOrPos.java
+++ src/ControlFlows/Leve1/CheckNumNegOrPos.java
@@ -1,9 +1,17 @@
 package ControlFlows.Leve1;
-
+import java.util.*;
 public class CheckNumNegOrPos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		if(n<0) {
+			System.out.println("Negative Number");
+		}else if(n==0) {
+			System.out.println("Zero");
+		}else {
+			System.out.println("Positive");
+		}
 
 	}
 
