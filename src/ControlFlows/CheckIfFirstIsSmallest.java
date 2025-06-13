7c6469586ac9a4a8252f0b6635c50fe0a5484545
Udbhav <101322451+Usri12@users.noreply.github.com>
2025-06-13 10:49:06 +0530

    Udbhav Srivastava check if the number is smallest among all

--- src/ControlFlows/CheckIfFirstIsSmallest.java
+++ src/ControlFlows/CheckIfFirstIsSmallest.java
@@ -1,10 +1,20 @@
 package ControlFlows;
-
+import java.util.*;
 public class CheckIfFirstIsSmallest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int num1 = sc.nextInt();
+		int num2 = sc.nextInt();
+		int num3 = sc.nextInt();
+		
+		boolean result = false;
+		if(num1 < num2 && num1 < num3) {
+			result = true;
+			System.out.println(result);
+		}else {
+			System.out.println(result);
+		}
 	}
 
 }
