ca6a7a7d0d27741830734eabe40caa1f0d9f157e
Udbhav <101322451+Usri12@users.noreply.github.com>
2025-06-13 09:59:59 +0530

    Udbhav Srivastava Day1 Level2 practice problems

---
+++ src/Day1/Level2/DistanceInMileAndyard.java
@@ -0,0 +1,16 @@
+package Day1.Level2;
+import java.util.*;
+public class DistanceInMileAndyard {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double distanceInFeet = sc.nextDouble();
+		double distanceInMiles = distanceInFeet/5280;
+		
+		System.out.println("Distance in Miles = "+distanceInMiles);
+		
+		
+
+	}
+
+}
